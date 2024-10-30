f81ffa0c1fb849b408d537d1671b4085e142bce1
André Rosatto <linus1178@gmail.com>
2024-10-30 12:49:34 -0300

    optimization

--- src/utils/grid.ts
+++ src/utils/grid.ts
@@ -1,6 +1,6 @@
 import { TilePosition } from "../typings/types";
 
-export function isPositionFree ({x, y, layer}: TilePosition, level: Array<TilePosition>): boolean {		
+export function isPositionFree({x, y, layer}: TilePosition, level: Array<TilePosition>): boolean {		
 	// check on top
 	if (level.some(t => t.layer - layer >= 1 && Math.abs(t.x - x) < 1 && Math.abs(t.y - y) < 1)) {
 		return false;
