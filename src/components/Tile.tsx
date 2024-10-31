be19c9dc5465b1326414d513e33cde662c439770
André Rosatto <linus1178@gmail.com>
2024-10-31 11:32:28 -0300

    optimizations

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -28,15 +28,10 @@ export interface TileProps {
 export default function Tile({pos, code, id, status, tileset, sizeX, onClick}: TileProps) {
   const style: {
 		base: CSSProperties,
-		bg: CSSProperties,
-		img: CSSProperties,
 		overlay: CSSProperties
 	} = {
     base: {
 			position: 'absolute',
-			boxSizing: 'content-box',
-			border: `1px solid ${status === 'selected' ? 'red' : 'rgba(0, 0, 0, .25)'}`,
-			borderRadius: '8%',
 			overflow: 'hidden',
 			transition: status === 'matched' ? 'transform .5s, opacity .5s .75s' : 'none',
 			transform: `
@@ -45,28 +40,14 @@ export default function Tile({pos, code, id, status, tileset, sizeX, onClick}: T
 			`,
 			transformOrigin: pos.x < sizeX / 2 ? 'top right' : 'top left',
       width: `calc(${100 / sizeX}% - 1.5px)`,
-			aspectRatio: 80 / (100 + DEPTH),
+			aspectRatio: 80 / 115,
 			cursor: status === 'free' || status === 'selected' ? 'pointer' : 'default',
 			zIndex: (pos.y * 2 + pos.x * 2 + pos.layer * (sizeX * 2 + SIZE_Y * 2)) * 10,
 			boxShadow: '0.5rem 0 2px rgba(0, 0, 0, .25)',
 			opacity: status === 'matched' ? 0 : 1,
 			pointerEvents: status === 'free' || status === 'selected' ? 'auto' : 'none',
-    },
-		bg: {
-			aspectRatio: 0.8,
-			position: 'absolute',
-			width: '100%',
-			background: 'linear-gradient(#ffedd5, #fed7aa)',
-			borderRadius: '8%',
-			boxShadow: `
-				0 ${DEPTH}px 0 #be7e29,
-				inset 0 2px 0 rgba(255, 255, 255, .25),
-				inset 0 -2px 0 rgba(0, 0, 0, .25)`
-		},
-    img: {
-      backgroundImage: `url(${tileset})`,
+			backgroundImage: `url(${tileset})`,
       backgroundPosition: `${code % 6 * 100 / 5}% ${Math.floor(code / 6) * 100 / 5}%`,
-			aspectRatio: 80 / 115,
 			backgroundSize: '600%'
     },
 		overlay: {
@@ -82,12 +63,6 @@ export default function Tile({pos, code, id, status, tileset, sizeX, onClick}: T
       style={style.base}
       onClick={() => {if (status === 'free' || status === 'selected') onClick(id)}}
     >
-			{/* background */}
-			<div style={style.bg}></div>
-
-      {/* image */}
-      <div style={style.img} className='absolute w-full' />
-
 			{/* overlay */}
 			{(status === 'selected' || status === 'blocked') && <div style={style.overlay}></div>}
     </div>
