28920f65618781b23bf84b41ba01443e27199624
André Rosatto <linus1178@gmail.com>
2024-11-04 21:23:08 -0300

    documentation

--- src/utils/randomizer.ts
+++ src/utils/randomizer.ts
@@ -1,3 +1,7 @@
+/**
+ * Pseudorandom number generator class using the Mulberry32 algorithm.
+ * @param seed The initial seed for the generator.
+ */
 export default class Randomizer {
 	private _generator = this.Mulberry32();
 	private _current;
@@ -19,19 +23,34 @@ export default class Randomizer {
 		}
 	}
 	
+	/**
+	 * Sets a new seed for the generator.
+	 * @param value The new seed.
+	*/
 	public set seed(value : number) {
 		this._seed = value;
 		this._current = value;
 	}
 	
+	/**
+	 * Gets the initial seed.
+	 * @returns The initial seed.
+	 */
 	public get seed() : number {
 		return this._seed;
 	}
 
+	/**
+	 * Resets the generator with the current seed.
+	 */
 	public reset(): void {
 		this._current = this._seed;
 	}
 
+	/**
+	 * Gets the next random number in the sequence.
+	 * @returns The next random number (between 0 and 1) in the sequence.
+	 */
 	public next (): number {
 		return this._generator();
 	}
